fb1162d39643eec08ebb6a2df2d5e39571111c86
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2023-11-27 06:59:43 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ let tvShowArray = [
 ]
 
 let animalsArray = [
-  "Dog",
-  "Cat",
-  "Parrot",
-  "Skunk"
+  "dog",
+  "cat",
+  "parrot",
+  "skunk"
 ];
 
 let statesArray = [
-  "Washington",
-  "California",
-  "Miami",
-  "Texas"
+  "washington",
+  "california",
+  "miami",
+  "texas"
 ]
 
 //Buttons, variables, and arrays 
@@ -38,6 +38,11 @@ let container2 = document.querySelector('.containerr');
 var chosenWord;
 var guess;
 let selectedLetters = [];
+let rightLetters = [];
+let wrongLetters = [];
+let numGuesses = 0;
+let guessedLetters;
+let guess1;
 
 function chooseWord(e){
    
@@ -58,8 +63,9 @@ function chooseWord(e){
 
       //Create elements based on the length of the chosen word
       //Print the elements to the screen
-      let word = document.createElement('input');
+      let word = document.createElement('hr');
       word.className = "word1";
+      word.id = "word";
       word.classList.add('style')
       movieContainer.appendChild(word);
       console.log(chosenWord);
@@ -70,18 +76,77 @@ function chooseWord(e){
     }
   } else if(buttonClick === "TV"){
       let random = Math.floor(Math.random() * tvShowArray.length);
-      chosenWord = movieArray[random];
+      chosenWord = tvShowArray[random];
       console.log(chosenWord);
+      chosenWord = chosenWord.split(' ');
+      chosenWord = chosenWord.join('');
+ 
+      
+       //Loop through the chosen word
+      for(let i = 1; i <= chosenWord.length; i++){
+ 
+       //Create elements based on the length of the chosen word
+       //Print the elements to the screen
+       let word = document.createElement('input');
+       word.className = "word1";
+       word.classList.add('style')
+       movieContainer.appendChild(word);
+       console.log(chosenWord);
+       categories.style.display = "none";
+       container.style.display = "none";
+       container2.style.display = "flex";
+       container2.classList.add('container0');
      }
-   else if(buttonClick === "animals"){
+    } else if(buttonClick === "animals"){
      let random = Math.floor(Math.random() * animalsArray.length);
      chosenWord = animalsArray[random];
      console.log(chosenWord);
+     console.log(chosenWord);
+      chosenWord = chosenWord.split(' ');
+      chosenWord = chosenWord.join('');
+ 
+      
+       //Loop through the chosen word
+      for(let i = 1; i <= chosenWord.length; i++){
+ 
+       //Create elements based on the length of the chosen word
+       //Print the elements to the screen
+       let word = document.createElement('input');
+       word.className = "word1";
+       word.classList.add('style')
+       movieContainer.appendChild(word);
+       console.log(chosenWord);
+       categories.style.display = "none";
+       container.style.display = "none";
+       container2.style.display = "flex";
+       container2.classList.add('container0');
+     }
    }
    else if(buttonClick === "states"){
     let random = Math.floor(Math.random() * statesArray.length);
     chosenWord = statesArray[random];
     console.log(chosenWord);
+
+    console.log(chosenWord);
+      chosenWord = chosenWord.split(' ');
+      chosenWord = chosenWord.join('');
+ 
+      
+       //Loop through the chosen word
+      for(let i = 1; i <= chosenWord.length; i++){
+ 
+       //Create elements based on the length of the chosen word
+       //Print the elements to the screen
+       let word = document.createElement('input');
+       word.className = "word1";
+       word.classList.add('style')
+       movieContainer.appendChild(word);
+       console.log(chosenWord);
+       categories.style.display = "none";
+       container.style.display = "none";
+       container2.style.display = "flex";
+       container2.classList.add('container0');
+     }
    }
   }
 
@@ -104,21 +169,38 @@ for(let i = 0; i < letters.length; i++){
 }
 
 function checkWord(e){
-      let buttonClick = e.target.dataset.id;
-      for(let i = 0; i < chosenWord.length; i++){
-     if(buttonClick === chosenWord[i]){
-       console.log('yes') 
-      return buttonClick;
-      
-     }
-
-     else
-     {
-      console.log('no')
-      return buttonClick;
+      let words = document.getElementsByClassName('word1');
+     //Check to see if the chosen word has the letter
+     //That the user has clicked
+     //If it does, console.log right letter
+      if(chosenWord.includes(e.target.dataset.id)){
+        console.log('right letter');
+        rightLetters.push(e.target.dataset.id);
+        console.log(rightLetters);
+        checkWin();
+        }
+     //Check to see if the chosen word does not have
+     //the letter that the user clicks
+     //If it doesn't, then console.log wrong letter
+     else if(!chosenWord.includes(e.target.dataset.id)){
+       console.log('wrong letter');
+       numGuesses++;
+       checkWin();
+       wrongLetters.push(e.target.dataset.id);
      }
       }
+
+//This function checks to see if the user guessed the word
+//Correctly
+function checkWin(){
+ if(chosenWord.length === rightLetters.length){
+  console.log('You win');
+ }
+ else if(numGuesses == 5){
+  console.log('you lose');
+ }
 }
+
 
 letters.forEach(item => {
  item.addEventListener('click', (e) => {
